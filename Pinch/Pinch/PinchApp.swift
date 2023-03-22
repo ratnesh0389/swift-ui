9e79534580c76bff62e7666e19f2afc58973012a
Ratnesh Singh <rksingh@calance.com>
2023-03-22 20:16:50 +0530

    3.Create new Project Pinch

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Ratnesh Singh on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
