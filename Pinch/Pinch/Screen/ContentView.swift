6cb2be5c31f0b6ac1d952c0ba02290f62fd24195
Ratnesh Singh <rksingh@calance.com>
2023-03-22 20:36:14 +0530

    Added navigation, title, image and animation

--- Pinch/Pinch/Screen/ContentView.swift
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -8,15 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Property
+    @State private var isAnimating: Bool = false
+    
+    // MARK: - Function
+    
+    //MMARK: - Content
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+//                    .animation(.linear(duration: 1), value: isAnimating)
+            }//ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            })
+        }//:NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
 
