adef2f48c1e1322bc111a4d19965c85f807c5bab
Ratnesh Singh <rksingh@calance.com>
2023-03-26 11:50:19 +0530

    Added demo code.

---
+++ AppleSwiftUI/AppleSwiftUI/AppleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSwiftUIApp.swift
+//  AppleSwiftUI
+//
+//  Created by Ratnesh Singh on 26/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleSwiftUI/AppleSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AppleSwiftUI
+//
+//  Created by Ratnesh Singh on 26/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
